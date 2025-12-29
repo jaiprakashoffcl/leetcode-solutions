7f56209274a8c7be97655caad0b6d12b0064982a
Jai Prakash R <126840211+jaiprakashoffcl@users.noreply.github.com>
2025-12-29 10:34:04 +0530

    Time: 1 ms (96.09%) | Memory: 42.4 MB (76.78%) - LeetSync

---
+++ 258-add-digits/add-digits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int addDigits(int num) {
+        while(num>=10){
+            int digit=0;
+            while(num>0){
+                digit += num%10;
+                num /=10;
+            }
+            num = digit;
+        }
+        return num;
+   }
+}
