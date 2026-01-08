87f43d7ec3f6d27981c4b5fdb7690b6263111983
Jai Prakash R <126840211+jaiprakashoffcl@users.noreply.github.com>
2026-01-08 11:13:01 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (22.72%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -5,7 +5,7 @@ class Solution {
             actualsum += nums[i];
         }
         int expectedsum=0;
-        for(int i=1;i<nums.length+1;i++){
+        for(int i=1;i<nums.length+1;i++){   //[1,n]
             expectedsum += i;
         }
         return expectedsum-actualsum;
